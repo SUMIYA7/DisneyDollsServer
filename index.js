9f2ddb5b7388876322a860b4e41896f44b0cee56
Sumiya Jakariya <sumiyajakariya38299@gmail.com>
2023-05-22 20:55:44 +0600

    added again

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function run() {
         res.send(result);
     });
 
-    // Send a Ping to confirm a successful connection
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
