87f965a8506d957b7c55ca5d69a6b1b9c21f9a3b
Sumiya Jakariya <sumiyajakariya38299@gmail.com>
2023-05-22 20:57:38 +0600

    changes

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish or error
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
